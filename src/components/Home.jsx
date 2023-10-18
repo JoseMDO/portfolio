141d2ccddbe931a108751e544f1780d6cb4dfae6
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-18 00:46:33 -0500

    updated message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,11 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-5xl sm:text-7xl font-bold text-black dark:text-white'>
-                    I'm a Full Stack <span className='text-gray-500 dark:text-[#14FECC]'>Developer</span>
+                    I'm a Software <span className='text-gray-500 dark:text-[#14FECC]'>Engineer</span>
                 </h2>
                 <p className='dark:text-gray-300 py-4 max-w-md text-black'>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    Welcome to My Portfolio
+                    I'm Jose Montes De Oca Morfin, and I'm on a journey to become a Software Engineer. Passionate about the world of code and problem-solving, I'm here to showcase my projects and experiences. Explore my work and discover how I'm turning my coding skills into solutions. Let's connect, innovate, and shape the future together.
                 </p>
                 <div>
                     <Link to="projects" smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-[#4b4949] to-[#b0b0b0] dark:from-[#14FFEC] dark:to-[#A0FFE3] dark:text-black cursor-pointer'>
